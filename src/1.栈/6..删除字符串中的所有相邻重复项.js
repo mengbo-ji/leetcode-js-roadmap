cedb8a33ae0a9461cf15c41ebf9d39637be3121b
mengbo.ji <mengbo.ji@tongdun.net>
2021-05-01 01:02:49 +0800

    feat: 算法学习3

---
+++ src/1.栈/6..删除字符串中的所有相邻重复项.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+const removeDuplicates = function(s) {
+  const stack = [];
+   for (let i = 0; i < s.length; i++) {
+       if (stack[stack.length - 1] !== s[i]) {
+           stack.push(s[i])
+       } else {
+           stack.pop();
+       }
+   }
+   return stack.join('');
+};
