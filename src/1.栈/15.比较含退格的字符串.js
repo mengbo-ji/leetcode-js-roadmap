85598fdcadf2d747cb88d9ab879f1649351ddbc6
mengbo.ji <mengbo.ji@tongdun.net>
2021-05-05 00:35:38 +0800

    feat: 算法学习14

---
+++ src/1.栈/15.比较含退格的字符串.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const backspaceCompare = function (s, t) {
+  const stackS = [];
+  const stackT = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '#') {
+      stackS.pop();
+    } else {
+      stackS.push(s[i])
+    }
+  }
+  for (let i = 0; i < t.length; i++) {
+    if (t[i] === '#') {
+      stackT.pop();
+    } else {
+      stackT.push(t[i])
+    }
+  }
+
+  return stackS.join() === stackT.join();
+};
