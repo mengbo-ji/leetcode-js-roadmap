df3b686e7eba12578cb5e029b759d428a3aacfff
mengbo.ji <mengbo.ji@tongdun.net>
2021-05-16 21:47:14 +0800

    feat: 算法-字符串学习4

---
+++ src/3.字符串/4.实现strStr().js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+const strStr = function (haystack, needle) {
+  if (needle === '') {
+    return 0;
+  }
+  if (haystack.length < needle.length) {
+    return -1;
+  }
+  let targetLength = needle.length;
+  for (let i = 0; i <= haystack.length - targetLength; i++) {
+    if (needle === haystack.substr(i, targetLength)) {
+      return i;
+    }
+  }
+  return -1;
+};
