69cdee96d128ebfb99b05fb05e2e657849971103
mengbo.ji <mengbo.ji@tongdun.net>
2021-05-19 23:36:51 +0800

    feat: 算法-字符串-学习9

---
+++ src/3.字符串/9.翻转字符串.js
@@ -0,0 +1,14 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+const reverseString = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    [s[left], s[right]] = [s[right], s[left]]
+    left++;
+    right--;
+  }
+  return s;
+};
