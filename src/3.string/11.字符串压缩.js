5e57122082f104ca91b015757d2969e4ac7cfea4
mengbo-ji <jmb33595@163.com>
2021-05-20 23:39:48 +0800

    feat: 算法-字符串-学习11

--- src/3.string/11.字符串压缩.js
+++ src/3.string/11.字符串压缩.js
@@ -1,2 +1,43 @@
-console.log('test')
-console.log('测试上ssh')
+/**
+ * @param {string} S
+ * @return {string}
+ */
+const compressString = function (s) {
+  let stack = [];
+  let res = '';
+  for (let i = 0; i < s.length; i++) {
+    if (!stack.length) {
+      stack.push(s[i]);
+      continue;
+    }
+    if (stack[stack.length - 1] === s[i]) {
+      stack.push(s[i]);
+    } else {
+      res += stack[0] + stack.length;
+      stack = [];
+      stack.push(s[i]);
+    }
+  }
+  res += stack[0] + stack.length;
+  return s.length > res.length ? res : s;
+};
+
+/**
+ * @param {string} S
+ * @return {string}
+ */
+const compressString = function (s) {
+  let res = '';
+  let count = 1;
+  let index = 0;
+  while (index < s.length) {
+    if (s[index] !== s[index + 1]) {
+      res += s[index] + count;
+      count = 1;
+    } else {
+      count++
+    }
+    index++
+  }
+  return s.length > res.length ? res : s;
+};
