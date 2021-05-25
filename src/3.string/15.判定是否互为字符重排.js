3387d78d018c978bbf560207de06c1b20cfc824f
mengbo-ji <jmb33595@163.com>
2021-05-25 22:59:59 +0800

    feat: 算法-字符串-学习15

---
+++ src/3.string/15.判定是否互为字符重排.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+const CheckPermutation = function (s1, s2) {
+  const res1 = s1.split('').sort((a, b) => {
+    return a.charCodeAt(0) - b.charCodeAt(0)
+  })
+  const res2 = s2.split('').sort((a, b) => {
+    return a.charCodeAt(0) - b.charCodeAt(0)
+  })
+  return res1.join('') === res2.join('');
+};
