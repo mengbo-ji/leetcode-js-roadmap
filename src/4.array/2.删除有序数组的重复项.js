45047e767fcd59251ea414e29d51641f012aa736
mengbo-ji <jmb33595@163.com>
2021-05-26 23:45:51 +0800

    feat: 算法-数组-学习2

---
+++ src/4.array/2.删除有序数组的重复项.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = function (nums) {
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
+};
