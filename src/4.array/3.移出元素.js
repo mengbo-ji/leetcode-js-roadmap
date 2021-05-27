708cf97c997d97e3d624f607fa8c0c561ce8eb29
mengbo-ji <jmb33595@163.com>
2021-05-27 15:13:11 +0800

    feat: 算法-数组-学习3

---
+++ src/4.array/3.移出元素.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
