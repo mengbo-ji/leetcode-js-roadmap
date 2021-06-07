bb23bfd299514cf247da568d35c6557f6e1ed800
mengbo-ji <jmb33595@163.com>
2021-06-07 16:50:10 +0800

    feat: 算法-数字-学习12

---
+++ src/4.array/12.数组中重复的数字.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findRepeatNumber = function (nums) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.get(nums[i]) || map.get(nums[i]) === 0) {
+      return nums[i]
+    } else {
+      map.set(nums[i], nums[i])
+    }
+  }
+};
