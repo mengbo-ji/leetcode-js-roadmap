ab3416f21ee99532ca5929ccb51b1de258bc7216
mengbo-ji <jmb33595@163.com>
2021-06-07 16:58:57 +0800

    feat: 算法-数字-学习13

---
+++ src/4.array/13.多数元素.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const majorityElement = function (nums) {
+  const len = nums.length / 2;
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const cur = nums[i];
+    const curLen = map.get(cur);
+    if (curLen) {
+      map.set(cur, curLen + 1);
+      if (map.get(cur) > len) {
+        return cur;
+      }
+    } else {
+      map.set(cur, 1);
+      if (map.get(cur) > len) {
+        return cur;
+      }
+    }
+  }
+};
