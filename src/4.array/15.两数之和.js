73c4732aff19b858cd928691f2a1b37501f320b5
mengbo-ji <jmb33595@163.com>
2021-06-08 19:52:59 +0800

    feat: 算法-数组-学习15

---
+++ src/4.array/15.两数之和.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (numbers, target) {
+  // for (let i = 0; i < numbers.length; i++) {
+  //     const cur = numbers[i];
+  //     for (let j = i + 1; j < numbers.length; j++) {
+  //         if (cur + numbers[j] === target) {
+  //             return [ i + 1, j + 1 ]
+  //         }
+  //     }
+  // }
+  const map = new Map();
+  for (let i = 0; i < numbers.length; i++) {
+    if (map.has(target - numbers[i])) {
+      return [map.get(target - numbers[i]), i + 1]
+    } else {
+      map.set(numbers[i], i + 1);
+    }
+  }
+};
