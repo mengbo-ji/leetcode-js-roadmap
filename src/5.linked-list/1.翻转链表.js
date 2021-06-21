7cb39ef903aacc1374cbff83bfcea82750fe7d33
mengbo-ji <jmb33595@163.com>
2021-06-21 11:12:42 +0800

    feat: 算法-链表-学习1

---
+++ src/5.linked-list/1.翻转链表.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function(head) {
+  let prev = null;
+  let cur = head;
+  while (cur) {
+    const next = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = next;
+  }
+  return prev;
+};
