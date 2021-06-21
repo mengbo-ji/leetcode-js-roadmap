6cd01d69523c4ec850256c44c08cb3efd5bd5f97
mengbo-ji <jmb33595@163.com>
2021-06-21 15:55:26 +0800

    feat: 算法-链表-学习2

---
+++ src/5.linked-list/2.合并两个有序的链表.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val);
+  this.next = (next === undefined ? null : next);
+}
+const mergeTwoLists = function(l1, l2) {
+  const prevHead = new ListNode(-1);
+  let prev = prevHead;
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      prev.next = l1;
+      l1 = l1.next;
+    } else {
+      prev.next = l2;
+      l2 = l2.next;
+    }
+    prev = prev.next;
+  }
+  prev.next = l1 ? l1 : l2;
+  return prevHead.next;
+};
