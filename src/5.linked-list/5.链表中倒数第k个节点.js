d2723f1967e4d9f4e24a92b498ab4262e6ae3052
mengbo-ji <jmb33595@163.com>
2021-06-23 11:28:38 +0800

    feat: 算法-链表-学习5

---
+++ src/5.linked-list/5.链表中倒数第k个节点.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+const getKthFromEnd = function(head, k) {
+  // const list = [];
+  // while(head) {
+  //     list.push(head.val);
+  //     head = head.next;
+  // }
+
+  // let temp = list.slice(-k);
+  // let newList = new ListNode();
+  // let prev = newList;
+  // temp.forEach(v => {
+  //     const cur = new ListNode(v);
+  //     prev.next = cur;
+  //     prev = prev.next;
+  // })
+  // return newList.next;
+  // 快慢指针法
+  let fast = slow = head;
+  let n = 0;
+  while (fast) {
+    if (n >= k) {
+      slow = slow.next;
+    }
+    fast = fast.next;
+    n++;
+  }
+  return slow;
+};
