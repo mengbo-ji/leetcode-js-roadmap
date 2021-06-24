208109d7b53d175e9f0473a524ecd77bde273c7c
mengbo-ji <jmb33595@163.com>
2021-06-24 13:46:55 +0800

    feat: 算法-链表-学习8

---
+++ src/5.linked-list/8.相交链表.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+const getIntersectionNode = function(headA, headB) {
+  const set = new Set();
+  while (headA) {
+    set.add(headA);
+    headA = headA.next;
+  }
+  while (headB) {
+    if (set.has(headB)) {
+      return headB;
+    }
+    headB = headB.next;
+  }
+  return null;
+};
