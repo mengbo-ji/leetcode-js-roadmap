a437ab6879509e5850f976ba26e27c75bf9bfa3f
mengbo-ji <jmb33595@163.com>
2021-06-27 16:59:35 +0800

    算法-链表-学习12

---
+++ src/5.linked-list/13.移出链表元素.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+const removeElements = function(head, val) {
+  const list = new ListNode(0);
+  list.next = head;
+  let temp = list;
+  while (temp.next) {
+    if (temp.next.val === val) {
+      temp.next = temp.next.next;
+    } else {
+      temp = temp.next;
+    }
+  }
+  return list.next;
+};
