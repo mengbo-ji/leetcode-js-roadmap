017f4797b9d8d78792e882df102cd7306fa42022
mengbo-ji <jmb33595@163.com>
2021-07-04 15:07:54 +0800

    feat: 算法-二叉树-学习3

---
+++ src/6.binary-tree/3.相同的树.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+const isSameTree = function(p, q) {
+  // 如果两个树都为空 返回 true
+  // 如果 一个树为空 一个树不为空 返回fasle
+  // 如果不为空 就判断他们的子节点是否相同
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+  if (p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
