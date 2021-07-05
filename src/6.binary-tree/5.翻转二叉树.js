5d79e00bc91c5fda738bce9a8ae6a56d07dd7304
mengbo-ji <jmb33595@163.com>
2021-07-05 22:01:14 +0800

    feat: 算法-链表-学习5

---
+++ src/6.binary-tree/5.翻转二叉树.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = function(root) {
+  // 终止条件：当前节点为 null 时返回
+  // 交换当前节点的左右节点，再递归的交换当前节点的左节点，递归的交换当前节点的右节点
+  if (!root) return null;
+  const left = invertTree(root.left);
+  const right = invertTree(root.right);
+  root.left = right;
+  root.right = left;
+  return root;
+};
