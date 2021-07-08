d2709f3014ce69544bb273ac0dddc99bffd047bc
mengbo-ji <jmb33595@163.com>
2021-07-08 17:02:59 +0800

    feat: 实现函数柯里化

--- src/lodash/5.add(1)(2)(3).js
+++ src/lodash/5.add(1)(2)(3).js
@@ -1,14 +1,12 @@
 
-// 计算函数，等收集到所有函数的参数之后组成数组传入进来
+// 计算函数，收集到所有函数的参数之后, 计算结果并返回
 const compute = (...args) => {
   return args.reduce((prev, next) => prev + next);
 };
 
-// 利用闭包，将计算函数先保存下来，fn = compute
 const currying = fn => {
-  // 收集函数的参数
+  // 利用闭包，将计算函数fn和参数数组args先保存下来，fn = compute, args 是所有函数传入进来的参数数组
   let args = [];
-  // 每次return 一个函数去继续收集下一个函数的参数
   return function temp(...newArgs) {
     // 如果参数存在 说明还要继续去添加到args中
     if (newArgs.length) {
@@ -16,11 +14,11 @@ const currying = fn => {
         ...args,
         ...newArgs,
       ];
-      // 继续去收集参数
+      // 将当前函数继续return 出去 继续去收集参数
       return temp;
     }
     // 如果参数不存在 说明 到了调用的时候()
-    // 那么这个时候只需要将收集到参数数组args 传入到计算函数并且返回值就好了
+    // 那么这个时候只需要将收集到参数数组 args 传入到计算函数并且将计算的值返回
     const res = fn.apply(this, args);
     args = []; // 将args重置为空
     return res;
