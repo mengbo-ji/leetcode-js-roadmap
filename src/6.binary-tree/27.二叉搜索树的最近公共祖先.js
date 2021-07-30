d5454f15be388ea95c7b84094a8343047424b41c
mengbo-ji <jmb33595@163.com>
2021-07-30 17:08:26 +0800

    feat: 算法-二叉树-学习27

---
+++ src/6.binary-tree/27.二叉搜索树的最近公共祖先.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const lowestCommonAncestor = function(root, p, q) {
+  if (p.val < root.val && q.val < root.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  }
+  if (p.val > root.val && q.val > root.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
+  return root;
+};
