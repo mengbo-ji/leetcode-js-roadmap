3598ce75f0967a9fa7681bd163a60c2b080a4120
mengbo-ji <jmb33595@163.com>
2021-09-26 22:45:49 +0800

    feat: 实现 对象的iterable接口

--- src/lodash/12.pointfree.js
+++ src/lodash/12.pointfree.js
@@ -8,3 +8,4 @@ const fp = require('lodash/fp');
 const f = fp.flowRight(fp.join('. '), fp.map(fp.flowRight(fp.toUpper, fp.first)), fp.split(' '));
 
 console.log(f('world wild web'));
+
---
+++ src/lodash/13.iterable.js
@@ -0,0 +1,69 @@
+// 实现对象的 iterable接口
+
+// const obj = {
+//   store: [ 'foo', 'bar', 'jmb' ],
+//   [Symbol.iterator]() {
+//     const self = this;
+//     let index = 0;
+//     return {
+//       next() {
+//         const result = {
+//           value: self.store[index],
+//           done: index >= self.store.length,
+//         };
+//         index++;
+//         return result;
+//       },
+//     };
+//   },
+// };
+
+// for (const item of obj) {
+//   console.log('item', item);
+// }
+
+
+const todos = {
+  life: [ '吃饭', '睡觉', '❤️' ],
+  learn: [ '学习', '敲代码', '提升' ],
+  work: [ '工作', '开会' ],
+  each(callback) {
+    const all = [ ...this.life, ...this.learn, ...this.work ];
+    for (const item of all) {
+      callback(item);
+    }
+  },
+  [Symbol.iterator]() {
+    const all = [ ...this.life, ...this.learn, ...this.work ];
+    let index = 0;
+    return {
+      next() {
+        const result = {
+          value: all[index],
+          done: index++ >= all.length,
+        };
+        return result;
+      },
+    };
+  },
+};
+
+// for (const item of todos.life) {
+//   console.log(item);
+// }
+
+// for (const item of todos.learn) {
+//   console.log(item);
+// }
+
+// for (const item of todos.work) {
+//   console.log(item);
+// }
+
+// todos.each(item => {
+//   console.log(item);
+// });
+
+for (const item of todos) {
+  console.log(item);
+}
